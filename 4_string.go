8236d11eb9abd52af5a0d8770aa77c9517364f71
Ilham <mi1797128@gmail.com>
2024-12-01 11:49:16 +0700

    string

---
+++ 4_string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhammad ")
+	fmt.Println("Muhammad Ilham ")
+	
+	fmt.Println(len("Muhammad Ilham"))
+	fmt.Println("Muhammad Ilham"[1])
+}
