decf21d9041736f2fa804e3ae5fc6b970b4b5ba6
Ilham <mi1797128@gmail.com>
2025-02-14 06:47:10 +0700

    feat: switch

---
+++ 15_switch.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	name := "ilham"
+
+	switch name {
+		case "ilham" : fmt.Println("hello ilham")
+		case "joko" : fmt.Println("hello joko")
+		default : fmt.Println("hello bukan ilham")
+	}
+
+
+}
