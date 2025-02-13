f6f993b1179c24ab82755db1e34e086136f037fd
Ilham <mi1797128@gmail.com>
2025-02-14 06:49:05 +0700

    feat: for expression

---
+++ 16_for_expression.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// counter := 0
+
+	// for counter < 10 {
+	// 	fmt.Println("Counter", counter)
+	// 	counter++
+	// }
+	
+	// best practice
+
+	for counter := 0; counter < 10; counter++ {
+		fmt.Println("Counter", counter)
+	}
+
+}
