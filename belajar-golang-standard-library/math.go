b4660d1a3f49432e0fb231b3715d657cad5cf78f
Ilham <mi1797128@gmail.com>
2025-03-22 14:12:58 +0700

    math

---
+++ belajar-golang-standard-library/math.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+
+	fmt.Println(math.Sin(2))
+	fmt.Println(math.Ceil(2.30))
+	fmt.Println(math.Floor(2.70))
+	fmt.Println(math.Round(2.70))
+	fmt.Println(math.Max(2,10))
+	fmt.Println(math.Min(2,10))
+}
